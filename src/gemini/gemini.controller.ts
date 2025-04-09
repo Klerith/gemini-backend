9364182341799fbd418f7f9d6ac7ee946c29d032
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-09 15:12:51 -0400

    feat: add basic prompt functionality with DTO validation and environment configuration

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
+import { BasicPromptDto } from './dtos/basic-prompt.dto';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
 
-  @Get()
-  getHelloWorld() {
-    return 'Hola Mundo';
+  @Post('basic-prompt')
+  basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
+    return this.geminiService.basicPrompt(basicPromptDto);
   }
 }
--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { BasicPromptDto } from './dtos/basic-prompt.dto';
 
 @Injectable()
-export class GeminiService {}
+export class GeminiService {
+  basicPrompt(basicPromptDto: BasicPromptDto) {
+    console.log(basicPromptDto);
+    return basicPromptDto;
+  }
+}
