15c3ebc6b9bdc692c1e465167676d5ccd2109edf
Fernando Herrera <fernando.herrera85@gmail.com>
2025-04-16 14:09:51 -0400

    feat: add ChatPromptDto and implement outputStreamResponse for streaming responses

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -13,10 +13,31 @@ import { Response } from 'express';
 
 import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { ChatPromptDto } from './dtos/chat-prompt.dto';
+import { GenerateContentResponse } from '@google/genai';
 
 @Controller('gemini')
 export class GeminiController {
   constructor(private readonly geminiService: GeminiService) {}
+
+  async outputStreamResponse(
+    res: Response,
+    stream: AsyncGenerator<GenerateContentResponse, any, any>,
+  ) {
+    // res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    let resultText = '';
+    for await (const chunk of stream) {
+      const piece = chunk.text;
+      resultText += piece;
+      res.write(piece);
+    }
+
+    res.end();
+    return resultText;
+  }
 
   @Post('basic-prompt')
   basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
@@ -33,17 +54,22 @@ export class GeminiController {
     basicPromptDto.files = files;
 
     const stream = await this.geminiService.basicPromptStream(basicPromptDto);
+    void this.outputStreamResponse(res, stream);
+  }
 
-    // res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(HttpStatus.OK);
+  @Post('chat-stream')
+  @UseInterceptors(FilesInterceptor('files'))
+  async chatStream(
+    @Body() chatPromptDto: ChatPromptDto,
+    @Res() res: Response,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
+    chatPromptDto.files = files;
 
-    for await (const chunk of stream) {
-      const piece = chunk.text;
-      console.log(piece);
-      res.write(piece);
-    }
+    const stream = await this.geminiService.basicPromptStream(chatPromptDto);
 
-    res.end();
+    const data = await this.outputStreamResponse(res, stream);
+
+    console.log({ data });
   }
 }
